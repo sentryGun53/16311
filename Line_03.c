eb64ed65b32f851cd905f60b9c5515b3b815aefd
sentryGun53 <sentryGun53@gmail.com>
2015-01-28 18:57:28 -0500

    Line_02.c is the last good tested version

--- Line_03.c
+++ Line_03.c
@@ -4,13 +4,13 @@
 #include "motion_functions.h"
 #include "math_functions.h"
 
-#define LIGHT_SENSOR_BLACK 38 // originally 37
-#define LIGHT_SENSOR_WHITE 62 // originally 53
+#define LIGHT_SENSOR_BLACK 36 // originally 37
+#define LIGHT_SENSOR_WHITE 58 // originally 53
 
-#define PIVOT_SPEED 18
-#define DEAD_ZONE_OFFSET 0.1
-#define MAX_SPEED 40
-#define MIN_SPEED 30
+#define PIVOT_SPEED 20
+#define DEAD_ZONE_OFFSET 0.04
+#define MAX_SPEED 35
+#define MIN_SPEED 25
 
 // uncomment this line to make cool noises
 //#define MELODIES
@@ -31,13 +31,13 @@ task main()
 			if (normalized_reading < 0.5 - DEAD_ZONE_OFFSET) {
 				// turn right proportionally
 				int off_center_amount = 0.5 - normalized_reading; // 0.0 to 0.5
-				int adjust_speed = pow(off_center_amount * 4, 2) * (MAX_SPEED - MIN_SPEED);
+				int adjust_speed = off_center_amount * 2 * (MAX_SPEED - MIN_SPEED);
 				motor[motorB] = MAX_SPEED;
 				motor[motorC] = (MAX_SPEED - adjust_speed);
 			} else if (normalized_reading > 0.5 + DEAD_ZONE_OFFSET) {
 				// turn left proportionally
-				int off_center_amount = 0.5 - normalized_reading; // 0.0 to 0.5
-				int adjust_speed = pow(off_center_amount * 4, 2) * (MAX_SPEED - MIN_SPEED);
+				int off_center_amount = normalized_reading - 0.5; // 0.0 to 0.5
+				int adjust_speed = off_center_amount * 2 * (MAX_SPEED - MIN_SPEED);
 				motor[motorB] = (MAX_SPEED - adjust_speed);
 				motor[motorC] = MAX_SPEED;
 			}
