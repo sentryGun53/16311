62e8ed369fd58821e9ddb0020dafa86da58d611b
haoala <haoala@gmail.com>
2015-01-28 23:59:46 -0500

    Calculates position and orientation to some degree of accuracy

--- Dead_01.c
+++ Dead_01.c
@@ -3,12 +3,20 @@
  * Written by Kaushik Viswanathan
  *****************************************/
 
+#define L 16.43 // wheel base in cm
+#define R 2.14 // wheel radius in cm
+
+#define PI 3.14159265359
+
 //Global variables - you will need to change some of these
 float robot_X = 0.0, robot_Y = 0.0, robot_TH = 0.0;
-int velocityUpdateInterval = 5;
+int velocityUpdateInterval = 5; // ms
 int PIDUpdateInterval = 2;
-int inputB[3] = {0, 0, 0};
-int inputC[3] = {0, 0, 0};
+int inputB[3] = {0,0,0};
+int inputC[3] = {0,0,0};
+
+float degrees_to_velocity = ((PI/180.0) / (velocityUpdateInterval/1000.0) * R); // units of cm/s
+int thetaBPrev = 0, thetaCPrev = 0;
 
 /*****************************************
  * Complete this function so that it
@@ -21,6 +29,24 @@ task dead_reckoning()
 		//
 		//Fill in code for numerical integration / position estimation here
 		//
+
+		int thetaBNow = nMotorEncoder[motorB];
+		int thetaCNow = nMotorEncoder[motorC];
+
+		int deltaThetaB = thetaBNow - thetaBPrev;
+		int deltaThetaC = thetaCNow - thetaCPrev;
+
+		float vRight = deltaThetaB * degrees_to_velocity; // Be careful here! Here we are assuming B is the right motor
+		float vLeft = deltaThetaC * degrees_to_velocity; // Swap deltaThetaB and deltaThetaC if motors are the other way round
+		float v = (vLeft + vRight)/2; // cm/s
+		float omega = (vRight - vLeft)/L;
+
+		robot_X = robot_X + v*cos(robot_TH) * (velocityUpdateInterval/1000.0);
+		robot_Y = robot_Y + v*sin(robot_TH) * (velocityUpdateInterval/1000.0);
+		robot_TH = robot_TH + omega * (velocityUpdateInterval/1000.0);
+
+		thetaBPrev = thetaBNow;
+		thetaCPrev = thetaCNow;
 
 		//Code that plots the robot's current position and also prints it out as text
 		nxtSetPixel(50 + (int)(100.0 * robot_X), 32 + (int)(100.0 * robot_Y));
@@ -98,20 +124,20 @@ void getInput()
 			if(inputB[i] > 100) {inputB[i] = 100; nMotorEncoder[motorA] = 100;}
 			else if(inputB[i] < -100) {inputB[i] = -100; nMotorEncoder[motorA] = -100;}
 			nxtDisplayTextLine(2 * i, "B : %d", inputB[i]);
-			wait10Msec(1);
+			wait1Msec(10);
 		}
-		wait10Msec(30);
+		wait1Msec(300);
 		while(nNxtButtonPressed != kEnterButton)
 		{
 			inputC[i] = (int)(nMotorEncoder[motorA] / 5.0);
 			if(inputC[i] > 100) {inputC[i] = 100; nMotorEncoder[motorA] = 100;}
 			else if(inputC[i] < -100) {inputC[i] = -100; nMotorEncoder[motorA] = -100;}
 			nxtDisplayTextLine(2 * i + 1, "C : %d", inputC[i]);
-			wait10Msec(1);
+			wait1Msec(10);
 		}
 		i++;
 		nMotorEncoder[motorA] = 0;
-		wait10Msec(30);
+		wait1Msec(300);
 	}
 	for(int j = 0; j < 8; j++)
 		nxtDisplayClearTextLine(j);
@@ -130,20 +156,23 @@ task main()
 	nMotorPIDSpeedCtrl[motorC] = mtrSpeedReg;
 	nPidUpdateInterval = PIDUpdateInterval;
 
-	getInput();
+	//getInput();
 
 	draw_grid();
 	time1[T1] = 0;
-	StartTask(dead_reckoning);
+	startTask(dead_reckoning);
+
 	for(int i = 0; i < 3; i++)
 	{
 		motor[motorB] = inputB[i];
 		motor[motorC] = inputC[i];
 
-		wait10Msec(100 * 5);
+		wait1Msec(1000 * 5);
   }
+
   motor[motorB] = 0;
 	motor[motorC] = 0;
 	nNxtButtonTask  = 0;
+
 	while(nNxtButtonPressed != kExitButton) {}
 }
