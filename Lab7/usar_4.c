90ff4ee5033b4f113f11d38ffba425f6e9a0fa5e
haoala <haoala@gmail.com>
2015-03-25 11:43:11 -0400

    usar_4: L2 and R2 have only one tread move forward

---
+++ Lab7/usar_4.c
@@ -0,0 +1,129 @@
+#pragma config(Sensor, S1,     lightSensor,    sensorLightActive)
+#pragma config(Sensor, S2,     nCompass,            sensorI2CHiTechnicCompass)
+#pragma DebuggerWindows("joystickSimple")
+
+#include "JoystickDriver.c"
+#include "motion_functions.h"
+#include "AdvancedSensors.c"
+
+// Button values
+// Button 1 = 1
+// Button 2 = 2
+// Button 3 = 4
+// Button 4 = 8
+// L1 = 16
+// L2 = 64
+// R1 = 32
+// R2 = 128
+// RtxL = 8
+// RtxR = 2
+
+#define L1 16
+#define L2 64
+#define LD 1024 // Left stick pressed
+#define R1 32
+#define R2 128
+#define B1 1
+#define B2 2
+#define B3 4
+#define B4 8
+
+// Line following constants
+#define LIGHT_SENSOR_THRESHOLD 30
+#define PIVOT_SPEED 10
+#define DEAD_ZONE_OFFSET 0.01
+#define MAX_SPEED 20
+#define MIN_SPEED 15
+
+
+bool goToDesiredPosition = false;
+int desiredPosition = 0;
+int gearConfiguration = -1;
+
+task goToDesiredPositionMotorA() {
+	while (true) {
+		int currentPosition = nMotorEncoder[motorA];
+		int difference = currentPosition - desiredPosition;
+		if (difference < -180) difference += 360;
+		else if (difference > 180) difference -= 360;
+
+		if (difference > 2) motor[motorA] = -75;
+		else if (difference < -2) motor[motorA] = 75;
+		else motor[motorA] = 0;
+		wait1Msec(10);
+	}
+}
+
+void turn_left() {
+	motor[motorB] = 50;
+	motor[motorC] = -50;
+}
+
+void turn_right() {
+	motor[motorB] = -50;
+	motor[motorC] = 50;
+}
+
+
+
+void followLine() {
+	while (joystick.joy1_Buttons == B1) {
+	  int sensor_reading = SensorValue[lightSensor];
+
+		if (sensor_reading < LIGHT_SENSOR_THRESHOLD) {
+			motor[motorB] = -50;
+			motor[motorC] = 25;
+		} else {
+			motor[motorB] = 25;
+			motor[motorC] = -50;
+		}
+
+		wait10Msec(1);
+		getJoystickSettings(joystick);
+	}
+}
+
+task playHeading() {
+	while (true) {
+		int nCompassHeading = SensorValue[nCompass];
+	}
+}
+
+task main()
+{
+	nPowerDownDelayMinutes = 525600;
+	nMotorEncoder[motorA] = 0;
+
+	startTask(playHeading);
+
+	while (true) {
+		getJoystickSettings(joystick);
+
+		nxtDisplayTextLine(0, "%d", joystick.joy1_x1);
+		nxtDisplayTextLine(1, "%d", joystick.joy1_y1);
+		nxtDisplayTextLine(2, "%d", joystick.joy1_Buttons);
+		nxtDisplayTextLine(3, "%d", joystick.joy2_Buttons);
+
+		if (joystick.joy1_Buttons == R1) turn_right();
+		else if (joystick.joy1_Buttons == L1) turn_left();
+		else if (joystick.joy1_Buttons == B1) followLine();
+		else if (joystick.joy1_x2 != 0) {
+			if (joystick.joy1_x2 < 0) motor[motorB] = 50 * joystick.joy1_x2/-128;
+			else motor[motorB] = 50 * joystick.joy1_x2/128;
+		} else {
+			float speedFactor = 1.0;
+			// Very slow
+			if (joystick.joy1_Buttons == L1 + L2) speedFactor = 0.2; // 10
+			// Fast
+			else if (joystick.joy1_Buttons == L1) speedFactor = 2; // 75
+			// Slow
+			else if (joystick.joy1_Buttons == L2) speedFactor = 0.5; // 25
+
+			motor[motorB] = (joystick.joy1_y1/127.0 * 50 + joystick.joy1_x1/127.0 * 50) * speedFactor * gearConfiguration;
+			motor[motorC] = (joystick.joy1_y1/127.0 * 50 - joystick.joy1_x1/127.0 * 50) * speedFactor * gearConfiguration;
+
+		}
+
+		wait10Msec(10);
+	}
+}
