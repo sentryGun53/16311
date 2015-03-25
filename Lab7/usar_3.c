165a38aa49ad4a0cfb9926b9e9597d68e49aade5
sentryGun53 <sentryGun53@gmail.com>
2015-03-25 11:25:08 -0400

    Merge remote-tracking branch 'origin/master'

--- Lab7/usar_3.c
+++ Lab7/usar_3.c
@@ -94,84 +94,34 @@ task main()
 		//motor[motorA] = -50; // Can only go backwards
 		getJoystickSettings(joystick);
 
-		if (joystick.joy1_Buttons == R1) turn_right();
-		else if (joystick.joy1_Buttons == L1) turn_left();
-		else if (joystick.joy1_Buttons == B1) followLine();
-
-		/*
-		if (joystick.joy1_Buttons == R1) {
-			wait1Msec(100);
-			if (goToDesiredPosition) {
-				nxtDisplayTextLine(4, "Yeah");
-				goToDesiredPosition = false;
-				stopTask(goToDesiredPositionMotorA);
-			} else {
-				nxtDisplayTextLine(4, "No");
-				goToDesiredPosition = true;
-				startTask(goToDesiredPositionMotorA);
-			}
-		}
-		*/
-
 		nxtDisplayTextLine(0, "%d", joystick.joy1_x1);
 		nxtDisplayTextLine(1, "%d", joystick.joy1_y1);
 		nxtDisplayTextLine(2, "%d", joystick.joy1_Buttons);
 		nxtDisplayTextLine(3, "%d", joystick.joy2_Buttons);
 
-		/*
-		if (joystick.joy1_Buttons == R1) motor[motorA] = -10;
-		else if (joystick.joy1_Buttons == R2) motor[motorA] = 10;
-		else motor[motorA] = 0;
-		*/
-
-		/*
-		if (joystick.joy1_Buttons == 4) {
-			stopTask(goToDesiredPositionMotorA);
-			goToDesiredPosition = false;
-			motor[motorA] = 20;
-			nMotorEncoder[motorA] = 0;
-		}
-		else if (joystick.joy1_Buttons == 1) {
-			stopTask(goToDesiredPositionMotorA);
-			goToDesiredPosition = false;
-			motor[motorA] = -20;
-			nMotorEncoder[motorA] = 0;
-		}
+		if (joystick.joy1_Buttons == R1) turn_right();
+		else if (joystick.joy1_Buttons == L1) turn_left();
+		else if (joystick.joy1_Buttons == B1) followLine();
 		else {
-			motor[motorA] = 0;
-			if (!goToDesiredPosition) {
-				startTask(goToDesiredPositionMotorA);
-				goToDesiredPosition = true;
-			}
+			float speedFactor = 1.0;
+			// Very slow
+			if (joystick.joy1_Buttons == L1 + L2) speedFactor = 0.2; // 10
+			// Fast
+			else if (joystick.joy1_Buttons == L1) speedFactor = 2; // 75
+			// Slow
+			else if (joystick.joy1_Buttons == L2) speedFactor = 0.5; // 25
+
+			motor[motorB] = (joystick.joy1_y1/127.0 * 50 + joystick.joy1_x1/127.0 * 50) * speedFactor * gearConfiguration;
+			motor[motorC] = (joystick.joy1_y1/127.0 * 50 - joystick.joy1_x1/127.0 * 50) * speedFactor * gearConfiguration;
 		}
-		*/
-
-
-
-		/*
-		if (joystick.joy1_Buttons == 8 || joystick.joy1_Buttons == 2 || joystick.joy1_Buttons == 24 || joystick.joy1_Buttons == 18) { // Right trigger is pressed left or right
-			// Turn
-			if (joystick.joy1_Buttons == 8) { // Turn left
-				motor[motorB] = -25 * gearConfiguration;
-				motor[motorC] = 25 * gearConfiguration;
-			} else if (joystick.joy1_Buttons == 24) { // 24 = 8 + 16 Turn left slowly
-				motor[motorB] = -10 * gearConfiguration;
-				motor[motorC] = 10 * gearConfiguration;
-			} else if (joystick.joy1_Buttons == 2) { // Turn right
-				motor[motorB] = 25 * gearConfiguration;
-				motor[motorC] = -25 * gearConfiguration;
-			} else { // Turn right slowly
-				motor[motorB] = 10 * gearConfiguration;
-				motor[motorC] = -10 * gearConfiguration;
-			}
-		}
+<<<<<<< HEAD
 		*/
 
 		float speedFactor = 1.0;
 		// Very slow
 		if (joystick.joy1_Buttons == L1 + L2) speedFactor = 0.2; // 10
 		// Fast
-		else if (joystick.joy1_Buttons == L1) speedFactor = 1.5; // 75
+		else if (joystick.joy1_Buttons == L1) speedFactor = 2.0; // 75
 		// Slow
 		else if (joystick.joy1_Buttons == L2) speedFactor = 0.5; // 25
 
@@ -199,6 +149,8 @@ task main()
 			motor[motorB] = 0;
 			motor[motorC] = 0;
 		}*/
+=======
+>>>>>>> origin/master
 
 		wait10Msec(1);
 	}
